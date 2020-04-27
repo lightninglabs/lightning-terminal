9b02ccfcee479082c0c8cacf44cc0cc43895005a
jamaljsr <jamal@farrock.com>
2020-04-27 13:53:16 -0400

    ui: update stories for the Loop page

--- app/src/__stories__/LoopPage.stories.tsx
+++ app/src/__stories__/LoopPage.stories.tsx
@@ -2,11 +2,13 @@ import React, { useEffect } from 'react';
 import { toJS } from 'mobx';
 import { StoryContext } from '@storybook/addons';
 import { Store } from 'store';
+import { Layout } from 'components/layout';
 import LoopPage from 'components/loop/LoopPage';
 
 export default {
   title: 'Loop Page',
   component: LoopPage,
+  parameters: { contained: true },
 };
 
 export const Default = (ctx: StoryContext) => {
@@ -15,7 +17,7 @@ export const Default = (ctx: StoryContext) => {
     const store = ctx.parameters.store as Store;
     const channels = toJS(store.channels);
     // only use a small set of channels
-    store.channels = channels.slice(0, 25);
+    store.channels = channels.slice(0, 10);
 
     // change back to sample data when the component is unmounted
     return () => {
@@ -27,3 +29,9 @@ export const Default = (ctx: StoryContext) => {
 };
 
 export const ManyChannels = () => <LoopPage />;
+
+export const InsideLayout = () => (
+  <Layout>
+    <LoopPage />
+  </Layout>
+);
