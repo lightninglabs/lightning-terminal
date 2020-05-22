10121157888742db80b18bbea67f71ae21162744
jamaljsr <jamal@farrock.com>
2020-05-22 15:14:39 -0400

    tests: update unit tests and stories

--- app/src/__stories__/SettingsPage.stories.tsx
+++ app/src/__stories__/SettingsPage.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useStore } from 'store';
 import { Layout } from 'components/layout';
 import SettingsPage from 'components/settings/SettingsPage';
 
@@ -9,6 +10,18 @@ export default {
 };
 
 export const Default = () => {
+  return <SettingsPage />;
+};
+
+export const BitcoinUnit = () => {
+  const { uiStore } = useStore();
+  uiStore.selectedSetting = 'unit';
+  return <SettingsPage />;
+};
+
+export const BalanceMode = () => {
+  const { uiStore } = useStore();
+  uiStore.selectedSetting = 'balance';
   return <SettingsPage />;
 };
 
