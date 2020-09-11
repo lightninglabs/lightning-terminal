27da903bac9adf7b7c3f2685295ae053587cad2a
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2020-09-11 13:02:15 -0400

    pool: add batchStore to fetch batch snapshots

--- app/src/store/stores/index.ts
+++ app/src/store/stores/index.ts
@@ -1,5 +1,6 @@
 export { default as AccountStore } from './accountStore';
 export { default as OrderStore } from './orderStore';
+export { default as BatchStore } from './batchStore';
 export { default as AuthStore } from './authStore';
 export { default as BuildSwapStore } from './buildSwapStore';
 export { default as ChannelStore } from './channelStore';
