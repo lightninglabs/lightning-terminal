97b71beea8d5be3e53e8494eb008caa9dfa2d1a3
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2020-11-03 23:22:50 -0500

    deps+mobx: remove outdated mobx-react-router

--- app/src/setupTests.ts
+++ app/src/setupTests.ts
@@ -1,5 +1,3 @@
-// https://github.com/mobxjs/mobx-react-lite/#observer-batching
-import 'mobx-react-lite/batchingForReactDom';
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
---
+++ app/src/store/stores/routerStore.ts
@@ -0,0 +1,30 @@
+import { makeAutoObservable, runInAction } from 'mobx';
+import { History, Location } from 'history';
+
+export default class RouterStore {
+  /** the history object */
+  history: History;
+
+  /** the current location from the browser's history */
+  location: Location;
+
+  constructor(history: History) {
+    makeAutoObservable(this, { history: false }, { deep: false, autoBind: true });
+
+    this.history = history;
+    this.location = history.location;
+
+    history.listen(location => {
+      runInAction(() => {
+        this.location = location;
+      });
+    });
+  }
+
+  /*
+   * History methods
+   */
+  push(location: string) {
+    this.history.push(location);
+  }
+}
