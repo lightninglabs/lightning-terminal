d93ac49999ce35c30b280e12280cd99251ab28a4
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2020-11-11 00:06:37 -0500

    pool+orders: add initial orders list

---
+++ app/src/components/pool/orders/OrderTable.tsx
@@ -0,0 +1,32 @@
+import { styled } from 'components/theme';
+
+export const Table = styled.table`
+  width: 100%;
+`;
+
+export const TableHeader = styled.th<{ right?: boolean }>`
+  font-size: ${props => props.theme.sizes.xs};
+  padding: 2px 10px;
+  text-align: ${props => (props.right ? 'right' : 'left')};
+
+  &:first-of-type {
+    padding-left: 0;
+  }
+
+  &:last-of-type {
+    padding-right: 0;
+  }
+`;
+
+export const TableRow = styled.tr<{ selectable?: boolean }>`
+  ${props =>
+    props.selectable &&
+    `  
+    &:hover {
+      cursor: pointer;
+      background-color: ${props.theme.colors.overlay};
+    }
+  `}
+`;
+
+export const TableCell = TableHeader.withComponent('td');
