43b9c59f28a5f8d7ba055bd92794316735c84b40
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2020-11-18 16:02:13 -0500

    pool+orders: add scrolling to order form on small screens

--- app/src/components/pool/OrderFormSection.tsx
+++ app/src/components/pool/OrderFormSection.tsx
@@ -4,7 +4,7 @@ import { usePrefixedTranslation } from 'hooks';
 import { Unit, Units } from 'util/constants';
 import { useStore } from 'store';
 import { Tier } from 'store/models/order';
-import { Button, Section, Small, SummaryItem } from 'components/base';
+import { Button, Scrollable, Section, Small, SummaryItem } from 'components/base';
 import FormField from 'components/common/FormField';
 import FormInputNumber from 'components/common/FormInputNumber';
 import FormSelect from 'components/common/FormSelect';
@@ -14,6 +14,13 @@ import { styled } from 'components/theme';
 const Styled = {
   Section: styled(Section)`
     flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 0;
+
+    > div {
+      padding: 15px;
+    }
   `,
   ApySummaryItem: styled(SummaryItem)`
     margin-top: 50px;
@@ -34,109 +41,111 @@ const OrderFormSection: React.FC = () => {
   const { Section, ApySummaryItem, Small, Actions } = Styled;
   return (
     <Section>
-      <Actions>
-        <Toggle
-          flex
-          options={orderFormView.orderOptions}
-          value={orderFormView.orderType}
-          onChange={orderFormView.setOrderType}
-        />
-      </Actions>
-      <FormField
-        label={l(`amountLabel${orderFormView.orderType}`)}
-        error={orderFormView.amountError}
-      >
-        <FormInputNumber
+      <Scrollable>
+        <Actions>
+          <Toggle
+            flex
+            options={orderFormView.orderOptions}
+            value={orderFormView.orderType}
+            onChange={orderFormView.setOrderType}
+          />
+        </Actions>
+        <FormField
           label={l(`amountLabel${orderFormView.orderType}`)}
-          placeholder={l('amountPlaceholder')}
-          extra={Units[Unit.sats].suffix}
-          value={orderFormView.amount}
-          onChange={orderFormView.setAmount}
-        />
-      </FormField>
-      <FormField
-        label={l(`premiumLabel${orderFormView.orderType}`)}
-        error={orderFormView.premiumError}
-      >
-        <FormInputNumber
-          label={l(`premiumLabel${orderFormView.orderType}`)}
-          placeholder={l('premiumPlaceholder')}
-          value={orderFormView.premium}
-          onChange={orderFormView.setPremium}
-          extra={
-            <>
-              <Button
-                ghost
-                borderless
-                compact
-                onClick={orderFormView.setSuggestedPremium}
-              >
-                {l('premiumSuggested')}
-              </Button>
-              <span>{Units[Unit.sats].suffix}</span>
-            </>
-          }
-        />
-      </FormField>
-      <FormField label={l('minChanSizeLabel')} error={orderFormView.minChanSizeError}>
-        <FormInputNumber
-          label={l('minChanSizeLabel')}
-          placeholder={l('minChanSizePlaceholder')}
-          extra={Units[Unit.sats].suffix}
-          value={orderFormView.minChanSize}
-          onChange={orderFormView.setMinChanSize}
-        />
-      </FormField>
-      <FormField label={l('feeLabel')} error={orderFormView.feeRateError}>
-        <FormInputNumber
-          label={l('feeLabel')}
-          placeholder={l('feePlaceholder')}
-          extra="sats/vbyte"
-          value={orderFormView.maxBatchFeeRate}
-          onChange={orderFormView.setMaxBatchFeeRate}
-        />
-      </FormField>
-      {orderFormView.orderType === 'Bid' && (
-        <FormField label={l('tierLabel')}>
-          <FormSelect
-            label={l('tierLabel')}
-            value={orderFormView.minNodeTier.toString()}
-            onChange={v => orderFormView.setMinNodeTier(parseInt(v) as Tier)}
-            options={orderFormView.nodeTierOptions}
+          error={orderFormView.amountError}
+        >
+          <FormInputNumber
+            label={l(`amountLabel${orderFormView.orderType}`)}
+            placeholder={l('amountPlaceholder')}
+            extra={Units[Unit.sats].suffix}
+            value={orderFormView.amount}
+            onChange={orderFormView.setAmount}
           />
         </FormField>
-      )}
-      <SummaryItem>
-        <span>{l('durationLabel')}</span>
-        <span className="text-right">
-          2016
-          <br />
-          <Small>(~{l('durationWeeks')})</Small>
-        </span>
-      </SummaryItem>
-      <SummaryItem>
-        <span>{l('fixedRateLabel')}</span>
-        <span>
-          {orderFormView.perBlockFixedRate < 1
-            ? `< 1`
-            : `${orderFormView.perBlockFixedRate}`}
-        </span>
-      </SummaryItem>
-      <ApySummaryItem strong>
-        <span>{l('apyLabel')}</span>
-        <span>{orderFormView.apy}%</span>
-      </ApySummaryItem>
-      <Actions>
-        <Button
-          primary={orderFormView.orderType === 'Bid'}
-          danger={orderFormView.orderType === 'Ask'}
-          ghost
-          disabled={!orderFormView.isValid}
-          onClick={orderFormView.placeOrder}
+        <FormField
+          label={l(`premiumLabel${orderFormView.orderType}`)}
+          error={orderFormView.premiumError}
         >
-          {orderFormView.placeOrderLabel}
-        </Button>
-      </Actions>
+          <FormInputNumber
+            label={l(`premiumLabel${orderFormView.orderType}`)}
+            placeholder={l('premiumPlaceholder')}
+            value={orderFormView.premium}
+            onChange={orderFormView.setPremium}
+            extra={
+              <>
+                <Button
+                  ghost
+                  borderless
+                  compact
+                  onClick={orderFormView.setSuggestedPremium}
+                >
+                  {l('premiumSuggested')}
+                </Button>
+                <span>{Units[Unit.sats].suffix}</span>
+              </>
+            }
+          />
+        </FormField>
+        <FormField label={l('minChanSizeLabel')} error={orderFormView.minChanSizeError}>
+          <FormInputNumber
+            label={l('minChanSizeLabel')}
+            placeholder={l('minChanSizePlaceholder')}
+            extra={Units[Unit.sats].suffix}
+            value={orderFormView.minChanSize}
+            onChange={orderFormView.setMinChanSize}
+          />
+        </FormField>
+        <FormField label={l('feeLabel')} error={orderFormView.feeRateError}>
+          <FormInputNumber
+            label={l('feeLabel')}
+            placeholder={l('feePlaceholder')}
+            extra="sats/vbyte"
+            value={orderFormView.maxBatchFeeRate}
+            onChange={orderFormView.setMaxBatchFeeRate}
+          />
+        </FormField>
+        {orderFormView.orderType === 'Bid' && (
+          <FormField label={l('tierLabel')}>
+            <FormSelect
+              label={l('tierLabel')}
+              value={orderFormView.minNodeTier.toString()}
+              onChange={v => orderFormView.setMinNodeTier(parseInt(v) as Tier)}
+              options={orderFormView.nodeTierOptions}
+            />
+          </FormField>
+        )}
+        <SummaryItem>
+          <span>{l('durationLabel')}</span>
+          <span className="text-right">
+            2016
+            <br />
+            <Small>(~{l('durationWeeks')})</Small>
+          </span>
+        </SummaryItem>
+        <SummaryItem>
+          <span>{l('fixedRateLabel')}</span>
+          <span>
+            {orderFormView.perBlockFixedRate < 1
+              ? `< 1`
+              : `${orderFormView.perBlockFixedRate}`}
+          </span>
+        </SummaryItem>
+        <ApySummaryItem strong>
+          <span>{l('apyLabel')}</span>
+          <span>{orderFormView.apy}%</span>
+        </ApySummaryItem>
+        <Actions>
+          <Button
+            primary={orderFormView.orderType === 'Bid'}
+            danger={orderFormView.orderType === 'Ask'}
+            ghost
+            disabled={!orderFormView.isValid}
+            onClick={orderFormView.placeOrder}
+          >
+            {orderFormView.placeOrderLabel}
+          </Button>
+        </Actions>
+      </Scrollable>
     </Section>
   );
 };
