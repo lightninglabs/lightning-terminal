0d4a6b2f633c135e4f533eac5eb5d3d5c73518a8
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2020-12-06 01:55:36 -0500

    tmp: remove unused code

--- app/src/components/pool/BatchSection.tsx
+++ app/src/components/pool/BatchSection.tsx
@@ -3,9 +3,6 @@ import { observer } from 'mobx-react-lite';
 import { Section } from 'components/base';
 import { styled } from 'components/theme';
 import BatchChart from './batches/BatchChart';
-import BatchD3Chart from './batches/BatchD3Chart';
-import BatchesChart from './batches/BatchesChart';
-import BatchList from './batches/BatchList';
 import BatchStats from './batches/BatchStats';
 
 const Styled = {
@@ -14,10 +11,6 @@ const Styled = {
     flex-direction: column;
     display: flex;
     background-color: transparent;
-
-    > div:last-of-type {
-      display: none;
-    }
   `,
 };
 
@@ -26,10 +19,7 @@ const BatchSection: React.FC = () => {
   return (
     <Section>
       <BatchStats />
-      <BatchesChart />
-      <BatchD3Chart />
       <BatchChart />
-      <BatchList />
     </Section>
   );
 };
