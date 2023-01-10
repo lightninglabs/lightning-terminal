7d26d2db1d3541d1ad8fe9bd24c783b72cb71649
Elle Mouton <elle.mouton@gmail.com>
2023-01-10 13:43:36 +0200

    litclient: add and export JSON Callback registration funcs
    This commit adds a new litclient package which can hold anything that
    LiT clients may want to import. In this commit, we add and export a list
    of JSON call back registration functions that a client will have access
    to when using Lit.

---
+++ litclient/jsoncallbacks.go
@@ -0,0 +1,44 @@
+package litclient
+
+import (
+	"context"
+
+	"github.com/lightninglabs/faraday/frdrpc"
+	"github.com/lightninglabs/loop/looprpc"
+	"github.com/lightninglabs/pool/poolrpc"
+	"github.com/lightningnetwork/lnd/lnrpc"
+	"github.com/lightningnetwork/lnd/lnrpc/autopilotrpc"
+	"github.com/lightningnetwork/lnd/lnrpc/chainrpc"
+	"github.com/lightningnetwork/lnd/lnrpc/invoicesrpc"
+	"github.com/lightningnetwork/lnd/lnrpc/routerrpc"
+	"github.com/lightningnetwork/lnd/lnrpc/signrpc"
+	"github.com/lightningnetwork/lnd/lnrpc/verrpc"
+	"github.com/lightningnetwork/lnd/lnrpc/walletrpc"
+	"github.com/lightningnetwork/lnd/lnrpc/watchtowerrpc"
+	"github.com/lightningnetwork/lnd/lnrpc/wtclientrpc"
+	"google.golang.org/grpc"
+)
+
+// StubPackageRegistration defines the signature of a function that maps JSON
+// method URIs to the function that should be called to handle the method.
+type StubPackageRegistration func(map[string]func(context.Context,
+	*grpc.ClientConn, string, func(string, error)))
+
+// Registrations defines a list of StubPackageRegistrations that a lit client
+// will have access to when using Lit.
+var Registrations = []StubPackageRegistration{
+	lnrpc.RegisterLightningJSONCallbacks,
+	lnrpc.RegisterStateJSONCallbacks,
+	autopilotrpc.RegisterAutopilotJSONCallbacks,
+	chainrpc.RegisterChainNotifierJSONCallbacks,
+	invoicesrpc.RegisterInvoicesJSONCallbacks,
+	routerrpc.RegisterRouterJSONCallbacks,
+	signrpc.RegisterSignerJSONCallbacks,
+	verrpc.RegisterVersionerJSONCallbacks,
+	walletrpc.RegisterWalletKitJSONCallbacks,
+	watchtowerrpc.RegisterWatchtowerJSONCallbacks,
+	wtclientrpc.RegisterWatchtowerClientJSONCallbacks,
+	looprpc.RegisterSwapClientJSONCallbacks,
+	poolrpc.RegisterTraderJSONCallbacks,
+	frdrpc.RegisterFaradayServerJSONCallbacks,
+}
