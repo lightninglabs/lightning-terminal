8db75cf11c4f0fc470605f04048a03a4fe1ea8c7
Elle Mouton <elle.mouton@gmail.com>
2023-05-21 18:47:09 +0200

    terminal: return errQueue error
    If an error is returned from the errQueue channel, we want that error to
    be returned so that LiT is shutdown with a non-0 exit code.

--- terminal.go
+++ terminal.go
@@ -622,8 +622,8 @@ func (g *LightningTerminal) start() error {
 	select {
 	case err := <-g.errQueue.ChanOut():
 		if err != nil {
-			log.Errorf("Received critical error from subsystem, "+
-				"shutting down: %v", err)
+			return fmt.Errorf("received critical error from "+
+				"subsystem, shutting down: %v", err)
 		}
 
 	case <-lndQuit:
