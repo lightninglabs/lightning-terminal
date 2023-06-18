e92350254f785d4b85b9145bca0b0f89977288b9
Elle Mouton <elle.mouton@gmail.com>
2023-06-18 13:08:18 +0200

    litrpc: add session link field to AddAutopilotSessionRequest
    Update the Autopilot service AddAutopilotSessionRequest message to
    include a new `group_id` field. Users may set this field to the ID of a
    sessino group in order to link the new session to the older ones. The
    Session message is also updated with new `group_id` field which will be
    populated by the group a session.

--- app/src/types/generated/lit-autopilot_pb.d.ts
+++ app/src/types/generated/lit-autopilot_pb.d.ts
@@ -27,6 +27,11 @@ export class AddAutopilotSessionRequest extends jspb.Message {
   getNoPrivacyMapper(): boolean;
   setNoPrivacyMapper(value: boolean): void;
 
+  getLinkedGroupId(): Uint8Array | string;
+  getLinkedGroupId_asU8(): Uint8Array;
+  getLinkedGroupId_asB64(): string;
+  setLinkedGroupId(value: Uint8Array | string): void;
+
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): AddAutopilotSessionRequest.AsObject;
   static toObject(includeInstance: boolean, msg: AddAutopilotSessionRequest): AddAutopilotSessionRequest.AsObject;
@@ -46,6 +51,7 @@ export namespace AddAutopilotSessionRequest {
     featuresMap: Array<[string, FeatureConfig.AsObject]>,
     sessionRules?: lit_sessions_pb.RulesMap.AsObject,
     noPrivacyMapper: boolean,
+    linkedGroupId: Uint8Array | string,
   }
 }
 
