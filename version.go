72cee9771071255e37c30a5f433c0db9ca381359
Viktor Tigerström <vtigerstrom@gmail.com>
2023-10-18 18:45:54 +0200

    version: bump version to `v0.12.0-alpha`

--- version.go
+++ version.go
@@ -22,7 +22,7 @@ const semanticAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqr
 // versioning 2.0.0 spec (http://semver.org/).
 const (
 	appMajor uint = 0
-	appMinor uint = 11
+	appMinor uint = 12
 	appPatch uint = 0
 
 	// appPreRelease MUST only contain characters from semanticAlphabet per
