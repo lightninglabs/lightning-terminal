b2e1314c9f5b3664c39ca525846b67498036711f
Oliver Gugger <gugger@gmail.com>
2024-05-23 13:56:41 +0200

    itest: add custom channel integration test
    Co-authored-by: Olaoluwa Osuntokun <laolu32@gmail.com>
    Co-authored-by: Gijs van Dam <gijs@lightning.engineering>
    Co-authored-by: George Tsagkarelis <george.tsagkarelis@gmail.com>

--- itest/litd_test_list_on_test.go
+++ itest/litd_test_list_on_test.go
@@ -24,4 +24,48 @@ var allTestCases = []*testCase{
 		name: "test large http header",
 		test: testLargeHttpHeader,
 	},
+	{
+		name: "test custom channels",
+		test: testCustomChannels,
+	},
+	{
+		name: "test custom channels large",
+		test: testCustomChannelsLarge,
+	},
+	{
+		name: "test custom channels grouped asset",
+		test: testCustomChannelsGroupedAsset,
+	},
+	{
+		name: "test custom channels force close",
+		test: testCustomChannelsForceClose,
+	},
+	{
+		name: "test custom channels breach",
+		test: testCustomChannelsBreach,
+	},
+	{
+		name: "test custom channels liquidity",
+		test: testCustomChannelsLiquidityEdgeCases,
+	},
+	{
+		name: "test custom channels htlc force close",
+		test: testCustomChannelsHtlcForceClose,
+	},
+	{
+		name: "test custom channels balance consistency",
+		test: testCustomChannelsBalanceConsistency,
+	},
+	{
+		name: "test custom channels single asset multi input",
+		test: testCustomChannelsSingleAssetMultiInput,
+	},
+	{
+		name: "test custom channels oracle pricing",
+		test: testCustomChannelsOraclePricing,
+	},
+	{
+		name: "test custom channels fee",
+		test: testCustomChannelsFee,
+	},
 }
