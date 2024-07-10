08366cbf3f5f0c66f36a20dc5c7c7be7714667f2
Olaoluwa Osuntokun <laolu32@gmail.com>
2024-07-09 22:11:52 -0700

    build: bump version to v0.13.99-alpha.rc3

--- version.go
+++ version.go
@@ -27,7 +27,7 @@ const (
 
 	// appPreRelease MUST only contain characters from semanticAlphabet per
 	// the semantic versioning spec.
-	appPreRelease = "alpha.rc2"
+	appPreRelease = "alpha.rc3"
 )
 
 // Version returns the application version as a properly formed string per the
