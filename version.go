b9e6bb5a8170e645998aed810eb8b5c3427c6132
Oliver Gugger <gugger@gmail.com>
2024-07-12 18:12:32 +0200

    build: bump version to v0.13.99-alpha.rc4

--- version.go
+++ version.go
@@ -27,7 +27,7 @@ const (
 
 	// appPreRelease MUST only contain characters from semanticAlphabet per
 	// the semantic versioning spec.
-	appPreRelease = "alpha.rc3"
+	appPreRelease = "alpha.rc4"
 )
 
 // Version returns the application version as a properly formed string per the
