20da83a1a76cd65de080e20e0dd8d9d6f8123499
Oliver Gugger <gugger@gmail.com>
2024-08-09 10:10:33 +0200

    version: bump version to v0.13.992-experimental

--- version.go
+++ version.go
@@ -23,7 +23,7 @@ const semanticAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqr
 const (
 	appMajor uint = 0
 	appMinor uint = 13
-	appPatch uint = 991
+	appPatch uint = 992
 
 	// appPreRelease MUST only contain characters from semanticAlphabet per
 	// the semantic versioning spec.
