824a3c259c84fbde77e24febcf6c17e0fc5c9253
Elle Mouton <elle.mouton@gmail.com>
2025-01-05 09:20:10 +0200

    make: add ability to specify unit test case
    This commit let's us specify commands of the form: `make unit
    case=TestNameHere`. Note that this commit by itself is not very useful
    in LiT since there are no tests in the main `terminal` package. A later
    commit will let us set a package name too and then this will be more
    useful.

--- make/testing_flags.mk
+++ make/testing_flags.mk
@@ -1,11 +1,17 @@
 include make/compile_flags.mk
 
 ITEST_FLAGS =
+TEST_FLAGS =
 
 # Define the integration test.run filter if the icase argument was provided.
 ifneq ($(icase),)
 ITEST_FLAGS += -test.run="TestLightningTerminal/$(icase)"
 endif
 
-UNIT := $(GOLIST) | $(XARGS) env $(GOTEST) -tags="$(COMPILE_TAGS)"
+# If a specific unit test case is being targeted, construct test.run filter.
+ifneq ($(case),)
+TEST_FLAGS += -test.run=$(case)
+endif
+
+UNIT := $(GOLIST) | $(XARGS) env $(GOTEST) -tags="$(COMPILE_TAGS)" $(TEST_FLAGS)
 UNIT_RACE := $(UNIT) -race
