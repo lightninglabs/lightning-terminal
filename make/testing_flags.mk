2b0ffff815039b32222511c39b03d859acc547d0
Elle Mouton <elle.mouton@gmail.com>
2025-01-05 09:22:37 +0200

    make: add ability to add additional tags
    This commit lets us perform commands of the form: `make unit
    tags=postgres` to add additional tags to the command. This will be
    useful in a future where we, for example, want to run unit tests against
    a specific DB backend.

--- make/testing_flags.mk
+++ make/testing_flags.mk
@@ -2,6 +2,7 @@ include make/compile_flags.mk
 
 ITEST_FLAGS =
 TEST_FLAGS =
+DEV_TAGS = dev
 
 # Define the integration test.run filter if the icase argument was provided.
 ifneq ($(icase),)
@@ -13,5 +14,10 @@ ifneq ($(case),)
 TEST_FLAGS += -test.run=$(case)
 endif
 
-UNIT := $(GOLIST) | $(XARGS) env $(GOTEST) -tags="$(COMPILE_TAGS)" $(TEST_FLAGS)
+# Add any additional tags that are passed in to make.
+ifneq ($(tags),)
+DEV_TAGS += ${tags}
+endif
+
+UNIT := $(GOLIST) | $(XARGS) env $(GOTEST) -tags="$(DEV_TAGS) $(COMPILE_TAGS)" $(TEST_FLAGS)
 UNIT_RACE := $(UNIT) -race
