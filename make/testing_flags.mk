43e1c9ee2d9f7455ffa4abc98146778a48d73434
Elle Mouton <elle.mouton@gmail.com>
2025-01-05 09:11:58 +0200

    make: move UNIT variables to testing_flags.mk
    And use the new COMPILE_TAGS variable for the unit commands instead of
    LND_RELEASE_TAGS.

--- make/testing_flags.mk
+++ make/testing_flags.mk
@@ -1,6 +1,11 @@
+include make/compile_flags.mk
+
 ITEST_FLAGS =
 
 # Define the integration test.run filter if the icase argument was provided.
 ifneq ($(icase),)
 ITEST_FLAGS += -test.run="TestLightningTerminal/$(icase)"
 endif
+
+UNIT := $(GOLIST) | $(XARGS) env $(GOTEST) -tags="$(COMPILE_TAGS)"
+UNIT_RACE := $(UNIT) -race
