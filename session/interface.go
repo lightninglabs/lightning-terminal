38afe620b97593874f4d66727d1b4bd582c9f248
Elle Mouton <elle.mouton@gmail.com>
2025-02-09 12:29:40 +0200

    session: remove the filter fn in ListSessions
    And instead let the caller pass in a list of States they are interested
    in. This will make SQL queries much more efficient since we can index by
    state.

--- session/interface.go
+++ session/interface.go
@@ -161,8 +161,10 @@ type Store interface {
 	// GetSession fetches the session with the given key.
 	GetSession(key *btcec.PublicKey) (*Session, error)
 
-	// ListSessions returns all sessions currently known to the store.
-	ListSessions(filterFn func(s *Session) bool) ([]*Session, error)
+	// ListSessions returns all sessions currently known to the store that
+	// are in the given states. If no states are provided, all sessions are
+	// returned.
+	ListSessions(states ...State) ([]*Session, error)
 
 	// ListSessionsByType returns all sessions of the given type.
 	ListSessionsByType(t Type) ([]*Session, error)
