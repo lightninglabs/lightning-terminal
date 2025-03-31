d533111db97d9953f34485bb411f592c1abcb077
ZZiigguurraatt <ZZiigguurraatt>
2025-03-31 17:14:13 -0400

    build: add build flags for other databases
    This commit adds support for all other databases
    as they are supported in the standalone lnd
    builds.

--- make/compile_flags.mk
+++ make/compile_flags.mk
@@ -1,1 +1,1 @@
-COMPILE_TAGS = autopilotrpc signrpc walletrpc chainrpc invoicesrpc watchtowerrpc neutrinorpc peersrpc
+COMPILE_TAGS = autopilotrpc signrpc walletrpc chainrpc invoicesrpc watchtowerrpc neutrinorpc peersrpc kvdb_postgres kvdb_etcd kvdb_sqlite
