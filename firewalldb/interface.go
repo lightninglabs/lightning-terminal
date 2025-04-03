5dd0479ed1e87cae451b52e871324f436ca9da3c
Elle Mouton <elle.mouton@gmail.com>
2025-04-03 08:35:55 +0200

    firewalldb: move bbolt kvstore logic to specific file
    Here, we move the abstract kvstore interfaces to the `interfaces.go`
    file and then rename the `kvstores.go` file to `kvstores_kvdb.go` to
    indicate that it houses the kvdb implementation of the kvstore
    interfaces. This is in preparation for adding a `kvstores_sql.go` file.

--- firewalldb/interface.go
+++ firewalldb/interface.go
@@ -35,3 +35,60 @@ type DBExecutor[T any] interface {
 	View(ctx context.Context, f func(ctx context.Context,
 		tx T) error) error
 }
+
+// KVStores provides an Update and View method that will allow the caller to
+// perform atomic read and write transactions on and of the key value stores
+// offered the KVStoreTx.
+type KVStores = DBExecutor[KVStoreTx]
+
+// KVStoreTx represents a database transaction that can be used for both read
+// and writes of the various different key value stores offered for the rule.
+type KVStoreTx interface {
+	// Global returns a persisted global, rule-name indexed, kv store. A
+	// rule with a given name will have access to this store independent of
+	// group ID or feature.
+	Global() KVStore
+
+	// Local returns a persisted local kv store for the rule. Depending on
+	// how the implementation is initialised, this will either be under the
+	// group ID namespace or the group ID _and_ feature name namespace.
+	Local() KVStore
+
+	// GlobalTemp is similar to the Global store except that its contents
+	// is cleared upon restart of the database. The reason persisting the
+	// temporary store changes instead of just keeping an in-memory store is
+	// that we can then guarantee atomicity if changes are made to both
+	// the permanent and temporary stores.
+	GlobalTemp() KVStore
+
+	// LocalTemp is similar to the Local store except that its contents is
+	// cleared upon restart of the database. The reason persisting the
+	// temporary store changes instead of just keeping an in-memory store is
+	// that we can then guarantee atomicity if changes are made to both
+	// the permanent and temporary stores.
+	LocalTemp() KVStore
+}
+
+// KVStore is in interface representing a key value store. It allows us to
+// abstract away the details of the data storage method.
+type KVStore interface {
+	// Get fetches the value under the given key from the underlying kv
+	// store. If no value is found, nil is returned.
+	Get(ctx context.Context, key string) ([]byte, error)
+
+	// Set sets the given key-value pair in the underlying kv store.
+	Set(ctx context.Context, key string, value []byte) error
+
+	// Del deletes the value under the given key in the underlying kv store.
+	Del(ctx context.Context, key string) error
+}
+
+// RulesDB can be used to initialise a new rules.KVStores.
+type RulesDB interface {
+	// GetKVStores constructs a new rules.KVStores in a namespace defined
+	// by the rule name, group ID and feature name.
+	GetKVStores(rule string, groupID session.ID, feature string) KVStores
+
+	// DeleteTempKVStores deletes all temporary kv stores.
+	DeleteTempKVStores(ctx context.Context) error
+}
