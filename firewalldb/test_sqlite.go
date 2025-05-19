d7ca6d3659db13e805e20cb12173a7ddb855a2d2
Viktor Tigerström <vtigerstrom@gmail.com>
2025-05-19 13:58:38 +0200

    firewalldb: update NewTestDB funcs to return FirewallDBs
    In the upcoming migration of the firewall database to SQL, the helper
    functions that creates the test databases of different types, need to
    return a unified interface in order to not have to control the
    migration tests file by build tags. Therefore, we update the
    `NewTestDB` functions to return the `FirewallDBs` interface instead of
    the specific store implementation type.

--- firewalldb/test_sqlite.go
+++ firewalldb/test_sqlite.go
@@ -10,13 +10,13 @@ import (
 )
 
 // NewTestDB is a helper function that creates an BBolt database for testing.
-func NewTestDB(t *testing.T, clock clock.Clock) *SQLDB {
+func NewTestDB(t *testing.T, clock clock.Clock) FirewallDBs {
 	return createStore(t, db.NewTestSqliteDB(t).BaseDB, clock)
 }
 
 // NewTestDBFromPath is a helper function that creates a new BoltStore with a
 // connection to an existing BBolt database for testing.
-func NewTestDBFromPath(t *testing.T, dbPath string, clock clock.Clock) *SQLDB {
+func NewTestDBFromPath(t *testing.T, dbPath string, clock clock.Clock) FirewallDBs {
 	return createStore(
 		t, db.NewTestSqliteDbHandleFromPath(t, dbPath).BaseDB, clock,
 	)
