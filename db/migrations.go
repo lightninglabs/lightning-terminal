08b91b09efc213d78b04407a0b90556449cc2a6b
Viktor Tigerström <vtigerstrom@gmail.com>
2025-07-24 02:39:36 +0200

    multi: introduce migration stream for unit tests
    In upcoming commits, we will introduce a new migration stream package
    that will need to reference the db package, as well as the accounts,
    session and firewalldb package in future commits. To avoid circular
    dependencies, we therefore introduce a new migration stream that unit
    tests can use, in order to avoid having to import the new migration
    stream package.

--- db/migrations.go
+++ db/migrations.go
@@ -1,5 +1,11 @@
 package db
 
+import (
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/pgx/v5"
+	"github.com/lightningnetwork/lnd/sqldb/v2"
+)
+
 const (
 	// LatestMigrationVersion is the latest migration version of the
 	// database. This is used to implement downgrade protection for the
@@ -8,3 +14,28 @@ const (
 	// NOTE: This MUST be updated when a new migration is added.
 	LatestMigrationVersion = 5
 )
+
+// MakeTestMigrationStreams creates the migration streams for the unit test
+// environment.
+func MakeTestMigrationStreams() []sqldb.MigrationStream {
+	migStream := sqldb.MigrationStream{
+		TrackingTableName: pgx.DefaultMigrationsTable,
+		SQLFileDirectory:  "sqlc/migrations",
+		SQLFiles:          SqlSchemas,
+
+		// LatestMigrationVersion is the latest migration version of the
+		// database.  This is used to implement downgrade protection for
+		// the daemon.
+		//
+		// NOTE: This MUST be updated when a new migration is added.
+		LatestMigrationVersion: LatestMigrationVersion,
+
+		MakeProgrammaticMigrations: func(db *sqldb.BaseDB) (
+			map[uint]migrate.ProgrammaticMigrEntry, error) {
+
+			return make(map[uint]migrate.ProgrammaticMigrEntry), nil
+		},
+	}
+
+	return []sqldb.MigrationStream{migStream}
+}
